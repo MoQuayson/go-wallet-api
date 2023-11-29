09d97890c841e478d087c0b97242944134bd69f9
Moses Quayson <mosesquayson1997@gmail.com>
2023-11-29 11:42:16 +0000

    added configurations for repository and services

---
+++ backend/config/repository.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"go-wallet-api/repositories"
+
+	"gorm.io/gorm"
+)
+
+/*
+This is where you register your repositories
+This is more like a DI style
+*/
+func RegisterRepositories(db *gorm.DB) {
+	repositories.NewUserRepository(db)
+	repositories.NewWalletRepository(db)
+}
---
+++ backend/config/service.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"go-wallet-api/repositories"
+	"go-wallet-api/services"
+)
+
+/*
+This is where you register your services
+This is more like a DI style
+*/
+func RegisterServices() {
+	services.NewUserService(repositories.GetUserRepository())
+	services.NewWalletService(repositories.GetWalletRepository())
+}
